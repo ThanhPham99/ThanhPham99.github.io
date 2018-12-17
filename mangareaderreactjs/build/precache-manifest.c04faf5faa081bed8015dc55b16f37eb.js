04819a31126d7ece7366bc38c1e6816262e6b151
Thanhpham99 <phamthanh110699@gmail.com>
2018-12-17 14:13:47 +0700

    test build react app

---
+++ mangareaderreactjs/build/precache-manifest.c04faf5faa081bed8015dc55b16f37eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a2b1273a92475bb9ed",
+    "url": "/static/css/main.884a368f.chunk.css"
+  },
+  {
+    "revision": "64a2b1273a92475bb9ed",
+    "url": "/static/js/main.64a2b127.chunk.js"
+  },
+  {
+    "revision": "0e113fbe21f964c60df2",
+    "url": "/static/js/1.0e113fbe.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c510f7aaacff9fad4883ca8a5776255",
+    "url": "/index.html"
+  }
+];
