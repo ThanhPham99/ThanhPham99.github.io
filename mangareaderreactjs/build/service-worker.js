cde0912dbd6a3e3bd557eb60c5d68e060f02322b
Thanhpham99 <phamthanh110699@gmail.com>
2018-12-17 16:04:13 +0700

    build reactjs app

--- mangareaderreactjs/build/service-worker.js
+++ mangareaderreactjs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c04faf5faa081bed8015dc55b16f37eb.js"
+  "/precache-manifest.3ee641fad8a34eaf36b8b093b5a0bc21.js"
 );
 
 workbox.clientsClaim();
