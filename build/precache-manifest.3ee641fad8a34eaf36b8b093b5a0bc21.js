237cde3180e0d905842c61e289d35ac69b2e8b68
Thanhpham99 <phamthanh110699@gmail.com>
2018-12-24 13:15:54 +0700

    m

---
+++ build/precache-manifest.3ee641fad8a34eaf36b8b093b5a0bc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4abeafeed2a8c04deb24",
+    "url": "/static/css/main.b3fa1cc4.chunk.css"
+  },
+  {
+    "revision": "4abeafeed2a8c04deb24",
+    "url": "/static/js/main.4abeafee.chunk.js"
+  },
+  {
+    "revision": "d370887d312dae8c4876",
+    "url": "/static/js/1.d370887d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9061948b87d28ed14a8c91ebbe76f27",
+    "url": "/index.html"
+  }
+];
